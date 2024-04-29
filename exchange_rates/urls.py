e8d7a1584d5916e4565624283ba1a03e517b15b2
Breno <bor@cin.ufpe.br>
2024-04-29 10:51:22 -0300

    feat: historical data and json optmized endpoints

--- exchange_rates/urls.py
+++ exchange_rates/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
 
-app_name = "exchange_rates"
+app_name = 'exchange_rates'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<str:base_currency>/<str:target_currency>/latest_rate/', views.LatestRateView.as_view(), name='latest_rate'),
-    path('<str:base_currency>/<str:target_currency>/historical_data/', views.HistoricalDataView.as_view(),
+    path('<str:base_currency>/<str:target_currency>/<str:value>', views.ExchangeRateView.as_view(),
+         name='exchange_rate'),
+    path('<str:base_currency>/<str:target_currency>/<str:value>/calc', views.calc_exchange, name='calc_exchange'),
+    path('latest_rate/<str:base_currency>/<str:target_currency>/', views.LatestRateView.as_view(), name='latest_rate'),
+    path('<str:base_currency>/<str:target_currency>/historical_data/<str:start_date>/<str:end_date>',
+         views.HistoricalDataView.as_view(),
          name='historical_data'),
+    path('<str:base_currency>/<str:target_currency>/historical_data/setup', views.set_history, name='change_range'),
+    path('historical_data/<str:base_currency>/<str:target_currency>/<str:start_date>/<str:end_date>',
+         views.HistoricalDataJSView.as_view(), name='historical_data_js'),
 ]
--- exchange_rates/views.py
+++ exchange_rates/views.py
@@ -1,7 +1,9 @@
-from django.db.models import F
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, Http404
+from django.core import serializers
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+from django.urls import reverse
 from django.views import generic
+
 from .models import Rate
 
 
@@ -21,14 +23,64 @@ class IndexView(generic.ListView):
         return latest_rates
 
 
-class LatestRateView(generic.ListView):
+class ExchangeRateView(generic.ListView):
     model = Rate
-    template_name = 'exchange_rates/latest_exchange.html'
-    context_object_name = 'latest_rate'
+    template_name = 'exchange_rates/exchange_rate.html'
+    context_object_name = 'info'
 
     def get_queryset(self):
-        rates = Rate.objects.filter(base_currency=self.kwargs['base_currency'],
-                                    target_currency=self.kwargs['target_currency'])
+        rates = Rate.objects.filter(
+            base_currency=self.kwargs['base_currency'],
+            target_currency=self.kwargs['target_currency'],
+        ).order_by('-timestamp')
+        if len(rates) < 1:
+            return {
+                'latest_rate': None,
+                'latest_rates': [],
+                'value': None,
+                'result': None,
+            }
+        latest_rates = Rate.objects.order_by('-timestamp')
+        exchanges = {}
+        for rate in latest_rates:
+            key = f'{rate.base_currency}->{rate.target_currency}'
+            if key not in exchanges:
+                exchanges[key] = rate.id
+            else:
+                latest_rates = latest_rates.exclude(id=rate.id)
+        return {
+            'latest_rate': rates[0],
+            'latest_rates': latest_rates,
+            'value': float(self.kwargs['value']),
+            'result': rates[0].rate * float(self.kwargs['value']),
+        }
+
+
+def calc_exchange(request, base_currency, target_currency, value):
+    if request.method == 'POST':
+        value = float(request.POST['BRL'].replace('$', '').replace(',', '.'))
+        return HttpResponseRedirect(
+            reverse(
+                'exchange_rates:exchange_rate',
+                args=(
+                    base_currency,
+                    target_currency,
+                    value,
+                ),
+            )
+        )
+
+
+class LatestRateView(generic.ListView):
+    model = Rate
+    template_name = 'exchange_rates/latest_rate.html'
+    context_object_name = 'rate'
+
+    def get_queryset(self):
+        rates = Rate.objects.filter(
+            base_currency=self.kwargs['base_currency'],
+            target_currency=self.kwargs['target_currency'],
+        )
         if len(rates) < 1:
             raise Rate.DoesNotExist
         timestamp = rates[0].timestamp
@@ -37,17 +89,131 @@ class LatestRateView(generic.ListView):
             if rate.timestamp > timestamp:
                 latest_rate = rate
                 timestamp = rate.timestamp
-        return latest_rate
+        rate_js = {
+            'base_currency': latest_rate.base_currency,
+            'target_currency': latest_rate.target_currency,
+            'rate': latest_rate.rate,
+            'timestamp': latest_rate.timestamp,
+        }
+        return rate_js
+
+
+def convert_tz(date_str):
+    return timezone.make_aware(timezone.datetime.strptime(date_str, '%Y-%m-%d'))
 
 
 class HistoricalDataView(generic.ListView):
     template_name = 'exchange_rates/historical_data.html'
-    context_object_name = 'rates'
+    context_object_name = 'info'
 
     def get_queryset(self):
-        rates = Rate.objects.filter(base_currency=self.kwargs['base_currency'],
-                                    target_currency=self.kwargs['target_currency'])
-        if len(rates) < 1:
-            raise Rate.DoesNotExist
+        start_date = self.kwargs['start_date']
+        end_date = self.kwargs['end_date']
+        rates = Rate.objects.filter(
+            base_currency=self.kwargs['base_currency'],
+            target_currency=self.kwargs['target_currency'],
+        ).order_by('timestamp')
+        if start_date == '0':
+            if len(rates) > 0:
+                start_date = rates[0].timestamp
+            else:
+                start_date = timezone.now()
+        else:
+            start_date = convert_tz(start_date)
+        if end_date == '0':
+            if len(rates) > 0:
+                end_date = rates[len(rates)-1].timestamp
+            else:
+                end_date = timezone.now()
+        else:
+            end_date = convert_tz(end_date)
+        if start_date > end_date:
+            aux = start_date
+            start_date = end_date
+            end_date = aux
 
-        return rates.order_by('-timestamp')
+        rates = Rate.objects.filter(
+            base_currency=self.kwargs['base_currency'],
+            target_currency=self.kwargs['target_currency'],
+            timestamp__gte=start_date,
+            timestamp__lte=end_date,
+        ).order_by('-timestamp')
+        latest_rates = Rate.objects.order_by('-timestamp')
+        exchanges = {}
+        for rate in latest_rates:
+            key = f'{rate.base_currency}->{rate.target_currency}'
+            if key not in exchanges:
+                exchanges[key] = rate.id
+            else:
+                latest_rates = latest_rates.exclude(id=rate.id)
+        return {'base': self.kwargs['base_currency'], 'target': self.kwargs['target_currency'], 'history': rates,
+                'latest_rates': latest_rates}
+
+
+def set_history(request, base_currency, target_currency):
+    if request.method == 'POST':
+        start = request.POST['start']
+        end = request.POST['end']
+        if not start or start == '':
+            start = '0'
+        if not end or end == '':
+            end = '0'
+        return HttpResponseRedirect(
+            reverse(
+                'exchange_rates:historical_data',
+                args=(
+                    base_currency,
+                    target_currency,
+                    start,
+                    end,
+                ),
+            )
+        )
+
+
+class HistoricalDataJSView(generic.ListView):
+    model = Rate
+    template_name = 'exchange_rates/historical_data_js.html'
+    context_object_name = 'history'
+
+    def get_queryset(self):
+        start_date = self.kwargs['start_date']
+        end_date = self.kwargs['end_date']
+        rates = Rate.objects.filter(
+            base_currency=self.kwargs['base_currency'],
+            target_currency=self.kwargs['target_currency'],
+        ).order_by('timestamp')
+        if start_date == '0':
+            if len(rates) > 0:
+                start_date = rates[0].timestamp
+            else:
+                start_date = timezone.now()
+        else:
+            start_date = convert_tz(start_date)
+        if end_date == '0':
+            if len(rates) > 0:
+                end_date = rates[len(rates)-1].timestamp
+            else:
+                end_date = timezone.now()
+        else:
+            end_date = convert_tz(end_date)
+        if start_date > end_date:
+            aux = start_date
+            start_date = end_date
+            end_date = aux
+
+        rates = Rate.objects.filter(
+            base_currency=self.kwargs['base_currency'],
+            target_currency=self.kwargs['target_currency'],
+            timestamp__gte=start_date,
+            timestamp__lte=end_date,
+        ).order_by('-timestamp')
+        rates_js = []
+        for rate in rates:
+            rates_js.append({
+                'base_currency': rate.base_currency,
+                'target_currency': rate.target_currency,
+                'rate': rate.rate,
+                'timestamp': rate.timestamp,
+            })
+        return rates_js
